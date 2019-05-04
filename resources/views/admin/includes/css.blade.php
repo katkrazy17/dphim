4b2a532c61104e61f303c42c22835f7acde16d2b
Trần Văn Thành <vanthanh1997qn@gmail.com>
2019-05-04 18:59:36 +0700

    fix css

--- resources/views/admin/includes/css.blade.php
+++ resources/views/admin/includes/css.blade.php
@@ -1,4 +1,2 @@
 <link href="{{ asset('vendor/css/app.css') }}" rel="stylesheet">
-<link href="{{ asset('vendor/css/fontawesome-all.min.css') }}" rel="stylesheet">
 <link href="{{ asset('vendor/css/signin.css') }}" rel="stylesheet">
-<link href="{{ asset('vendor/css/dashboard.css') }}" rel="stylesheet">
